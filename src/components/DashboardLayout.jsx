d6a8b9dd098d6b15b9df5c377b75c7356e23e210
sajid9962 <clashking1545@gmail.com>
2024-06-15 10:59:54 +0600

    component cleanup done

--- src/components/DashboardLayout.jsx
+++ src/components/DashboardLayout.jsx
@@ -21,14 +21,10 @@ const DashboardLayout = () => {
 			// Unbind the event listener on clean up
 			document.removeEventListener("mousedown", handleClickOutside);
 		};
-	}, [wrapperRef]);
-
-	
+	}, []);
 
 	const role = "teacher";
-
 	const Menus = sidebarRoutesGenerator(role);
-
 	const pathname = useLocation().pathname;
 
 	return (
@@ -36,40 +32,32 @@ const DashboardLayout = () => {
 			{/* Sidebar */}
 			<div
 				ref={wrapperRef}
-				className={` ${
-					open ? "w-72" : "w-20 "
-				} bg-gradient-to-b from-blue-700 via-blue-800 to-gray-900 text-white h-screen p-2 lg:p-5  pt-8 relative duration-300 min-h-svh`}>
+				className={`${
+					open ? "w-72" : "w-20"
+				} bg-gradient-to-b from-blue-700 via-blue-800 to-gray-900 text-white h-screen p-2 lg:p-5 pt-8 relative duration-300 min-h-svh`}>
 				<RxCaretLeft
 					color="black"
 					size={30}
-					className={`absolute cursor-pointer -right-3 top-9 w-7 rounded-full border-2  z-50   bg-white    ${
-						!open && "rotate-180"
-					}`}
+					className={`absolute cursor-pointer -right-3 top-9 w-7 rounded-full border-2 z-50 bg-white ${!open && "rotate-180"}`}
 					onClick={() => setOpen(!open)}
+					aria-label="Toggle Sidebar"
 				/>
 
 				<div className="flex gap-x-4 items-center">
-					<img src={logo} className={`cursor-pointer duration-500 ${open && "rotate-[360deg]"}`} />
-					<h1
-						className={`text-white origin-left font-medium text-xl duration-200 ${
-							!open && "scale-0"
-						}`}>
+					<img src={logo} className={`cursor-pointer duration-500 ${open && "rotate-[360deg]"}`} alt="Logo" />
+					<h1 className={`text-white origin-left font-medium text-xl duration-200 ${!open && "scale-0"}`}>
 						Designer
 					</h1>
 				</div>
 				<ul className="pt-6">
 					{Menus.map((Menu, index) => (
-						<Link title={Menu.title} className="" key={index} to={Menu.to}>
+						<Link title={Menu.title} key={index} to={Menu.to}>
 							<li
-								key={index}
-								className={`flex  rounded-md p-2 cursor-pointer ${
+								className={`flex rounded-md p-2 cursor-pointer ${!open && "justify-center"} ${
 									pathname === Menu.to ? "bg-light-white" : ""
-								}  text-gray-300 text-sm items-center gap-x-4 
-              ${Menu.gap ? "mt-9" : "mt-2"}  `}>
+								} text-gray-300 text-sm items-center gap-x-4 ${Menu.gap ? "mt-9" : "mt-2"}`}>
 								{Menu.src}
-								<span className={`${!open && "hidden"} origin-left duration-200`}>
-									{Menu.title}
-								</span>
+								<span className={`${!open && "hidden"} origin-left duration-200`}>{Menu.title}</span>
 							</li>
 						</Link>
 					))}
@@ -78,13 +66,15 @@ const DashboardLayout = () => {
 
 			<div className="w-full">
 				<DashboardNavbar />
-
 				{/* Body */}
 				<div className="flex-1 p-7">
-					<h1 className="text-2xl font-semibold ">{<Outlet />}</h1>
+					<h1 className="text-2xl font-semibold">
+						<Outlet />
+					</h1>
 				</div>
 			</div>
 		</div>
 	);
 };
+
 export default DashboardLayout;
